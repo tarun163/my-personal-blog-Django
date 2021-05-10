30ebb77bb3702960450c1426c91873baf4251ea2
tarun kumar <2019163@iiitdmj.ac.in>
2021-05-10 17:45:59 +0530

    comment box added

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
@@ -8,3 +8,14 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title','content']
     prepopulated_fields = {'slug':('title',)}
 admin.site.register(Post,PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name','body','post','created_on','active')
+    list_filter = ('active','created_on')
+    search_field = ('name','email','body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+        
+admin.site.register(Comment,CommentAdmin )
--- home/forms.py
+++ home/forms.py
@@ -3,10 +3,15 @@ from django.contrib.auth.forms import User
 
 from django.contrib.auth.forms import UserCreationForm 
 from django import forms
-
+from .models import Comment
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','first_name','last_name','email','password1','password2']        
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name','email','body')        
